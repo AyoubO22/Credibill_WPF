9d913199463d2c89d7ce061eef708c564e5908c7
Ayoub Ouaadoud <ayoub.ouaadoud@student.ehb.be>
2024-11-23 13:39:21 +0100

    x

--- Credibill/MainWindow.xaml.cs
+++ Credibill/MainWindow.xaml.cs
@@ -1,6 +1,5 @@
-﻿using System;
+﻿using CrediBill_WPF.Views;
 using System.Windows;
-using CrediBill_WPF.Views;
 
 namespace CrediBill_WPF
 {
@@ -11,6 +10,50 @@ namespace CrediBill_WPF
             InitializeComponent();
         }
 
+        // Actions pour le menu "File"
+        private void Exit_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown(); // Quitter l'application
+        }
+
+        private void NewFile_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("New File clicked!");
+        }
+
+        private void OpenFile_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Open File clicked!");
+        }
+
+        private void SaveFile_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Save File clicked!");
+        }
+
+        // Actions pour le menu "Edit"
+        private void Cut_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Cut clicked!");
+        }
+
+        private void Copy_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Copy clicked!");
+        }
+
+        private void Paste_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Paste clicked!");
+        }
+
+        // Action pour le menu "Help"
+        private void About_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("About clicked! CrediBill - Invoice and Payment Management");
+        }
+
+        // Actions pour les boutons de navigation
         private void NavigateToCustomerView(object sender, RoutedEventArgs e)
         {
             MainFrame.Navigate(new CustomerView());
@@ -25,6 +68,5 @@ namespace CrediBill_WPF
         {
             MainFrame.Navigate(new PaymentView());
         }
-
     }
 }
